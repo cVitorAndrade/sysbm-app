0fde8297ca93e9de8c78407eec1e8f701c112082
Thamires <thamires.taboza@gmail.com>
2024-12-17 09:36:12 -0300

    fix: breadcrumb changed

--- src/components/Breadcrumb/index.tsx
+++ src/components/Breadcrumb/index.tsx
@@ -2,30 +2,15 @@ import { IoIosArrowForward } from 'react-icons/io';
 import { Link, useLocation } from 'react-router-dom';
 import { Container } from './styles';
 
-// Definição do tipo das props
-interface BreadcrumbProps {
-  path: string[]; // Torna o path opcional
-}
-
-export default function Breadcrumb({ path = [] }: BreadcrumbProps) {
-  const location = useLocation(); // Mantém a lógica de localização
+export default function Breadcrumb() {
+  const location = useLocation();
   const isRegisterPage = location.pathname === '/ListReaders';
 
   return (
     <Container>
-      {/* HOME sempre será o primeiro */}
       <Link to="/" style={{ color: isRegisterPage ? '#000' : '#fff' }}>
-        <IoIosArrowForward />
+        HOME <IoIosArrowForward />
       </Link>
-
-      {/* Renderiza os demais itens do path */}
-      {path.map((item, index) => (
-        // eslint-disable-next-line react/no-array-index-key
-        <span key={index}>
-          <Link to={`/${item.toLowerCase()}`}>{item}</Link>
-          {index < path.length - 1 && <IoIosArrowForward />}
-        </span>
-      ))}
     </Container>
   );
 }
